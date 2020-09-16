11b3a2ef89561bde7fc1661ca1f6132e821b4099
Andres Ruiz <rb.andres98@gmail.com>
2020-09-16 08:33:50 -0500

    Init Dart project

---
+++ example/colombian_departments_example.dart
@@ -0,0 +1,6 @@
+import 'package:colombian_departments/colombian_departments.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/colombian_departments.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library colombian_departments;
+
+export 'src/colombian_departments_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
